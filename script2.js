34f8b4fcd5b7c4e2cdcb586db48a8d77dca96603
Adriel Maximus <adrvlogs885@gmail.com>
2023-04-26 20:06:33 -0300

    a

--- script2.js
+++ script2.js
@@ -1,7 +1,16 @@
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('ativo');
+  NavMenu.classList.toggle('ativo');
+});
 
 const balanceAmount = document.getElementById('balance-amount');
 const transactionsList = document.getElementById('transactions-list');
 const addTransactionForm = document.getElementById('add-transaction-form');
+const removeTransactionButton = document.getElementById('remove-transaction-button');
+const clearTransactionsButton = document.getElementById('clear-transactions-button');
 
 let balance = 0;
 let transactions = [];
@@ -12,12 +21,32 @@ function addTransaction(event) {
   const descriptionInput = document.getElementById('transaction-description');
   const amount = parseFloat(amountInput.value);
   const description = descriptionInput.value;
+
+  // Check if amount is a valid number
+  if (isNaN(amount)) {
+    amountInput.value = '';
+    descriptionInput.value = '';
+    return;
+  }
+
   const transaction = { amount, description };
   transactions.push(transaction);
   updateTransactions();
   updateBalance();
   amountInput.value = '';
   descriptionInput.value = '';
+}
+
+function removeLastTransaction() {
+  transactions.pop();
+  updateTransactions();
+  updateBalance();
+}
+
+function clearTransactions() {
+  transactions = [];
+  updateTransactions();
+  updateBalance();
 }
 
 function updateTransactions() {
@@ -36,18 +65,27 @@ function updateTransactions() {
   }
 }
 
+
 function updateBalance() {
   balance = 0;
   for (let i = 0; i < transactions.length; i++) {
     const transaction = transactions[i];
     balance += transaction.amount;
   }
-  balanceAmount.textContent = 'R$' + balance.toFixed(2);
-  if (balance < 0) {
-    balanceAmount.style.color = 'red';
+  if (isNaN(balance)) {
+    balanceAmount.style.display = 'none'; // hide balance if it's NaN
   } else {
-    balanceAmount.style.color = 'black';
+    balanceAmount.style.display = 'block'; // show balance if it's valid
+    balanceAmount.textContent = '$' + balance.toFixed(2);
+    if (balance >= 0) {
+      balanceAmount.style.color = 'green';
+    } else {
+      balanceAmount.style.color = 'red';
+    }
   }
 }
 
+
 addTransactionForm.addEventListener('submit', addTransaction);
+removeTransactionButton.addEventListener('click', removeLastTransaction);
+clearTransactionsButton.addEventListener('click', clearTransactions);
